14b5a2181d1a9fa4aa2ddfb87fa9bc4b43a98ead
MihailVK04 <149954001+MihailVK04@users.noreply.github.com>
2026-07-30 21:12:48 +0300

    Create 2019-SE-02.sh

---
+++ Student book task/Shell/Scripts/2019-SE-02.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ ${#} -eq 0 ]]; then
+    echo "The script expcts parameters"
+    exit 1
+fi
+N=10
+if [[ ${1} == "-n" ]]; then
+    if [[! ${2} ~= ^[1-9]+[0-9]*$ ]]; then
+        echo "When the first param is -n the second must be a num"
+        exit 2
+    fi
+    N=${2}
+    shift 2
+fi
+allDates="$(mktemp)"
+
+for file in "${@}"; do
+    idf=$(basename ${file} | sed -E s:".log"::g)
+    while read -r line; do
+        info=$(echo "${line}" | cut -d ' ' -f3-)
+        date=$(echo "${line}" | cut -d ' ' -f1,2)
+        echo "${date} ${idf} ${info}" >> ${allDates}
+    done< <(cat ${file})
+done
+cat ${allDates} | sort -t " " -k1 -k2 | tail -n ${N}
+rm ${allDates}        
