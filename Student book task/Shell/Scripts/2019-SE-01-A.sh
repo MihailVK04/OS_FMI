cbbe3c40de0c08cea37180436f727f95d60bfb3e
MihailVK04 <149954001+MihailVK04@users.noreply.github.com>
2026-07-30 20:36:15 +0300

    Create 2019-SE-01-A.sh

---
+++ Student book task/Shell/Scripts/2019-SE-01-A.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+maxValue=0;
+
+while read -r line; do
+    if [[ ${line} =~ ^[1-9]+[1-9]*$ ]]; then
+        if [[ ${line} -gt ${maxValue} ]]; then
+            maxValue=${line}
+        fi
+    fi
+done< <(cat ${1} | tr -d '-')
+cat ${1} | grep -E "^-?${maxValue}" | sort | uniq
