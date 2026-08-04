6bed8399e91561913a735af3c4d5ae03380e3bba
MihailVK04 <149954001+MihailVK04@users.noreply.github.com>
2026-08-04 23:50:10 +0300

    Create 2025-SE-01.sh

---
+++ Student book task/Shell/Scripts/2025-SE-01.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+if [[ ${#} -ne 2 ]];then
+    echo "args must be 2"
+    exit 1
+fi
+if [[ ! -f ${1} ]]; then
+    echo "file 1 doesnt exist"
+    exit 2
+fi
+if [[ ! -f ${2} ]]; then
+    echo "file 2 doesnt exist"
+    exit 3
+fi
+
+if [[ ${1} =~ ^- ]]; then
+    echo "file 1 ${1} cannot start with -"
+    exit 4
+fi
+if [[ ${2} =~ ^- ]];then
+    echo "file 2 ${2} cannot start with -"
+    exit 5
+fi
+if [[ ! ${1} =~ [:alnum:] ]]; then
+    echo "file 1 contains special characters Error"
+    exit 6
+fi
+if [[ ! ${2} =~ [:alnum:] ]]; then
+    echo "file 2 contains special characters Error"
+    exit 7
+fi
+commands=$(mktemp)
+while read -r line; do
+    language=$(echo "${line}" | tr -s ' ' |cut -d ' ' -f1)
+    echo "Language: ${language}"
+    options=$(echo "${line}" | tr -s ' ' |cut -d ' ' -f2- | sed -E "s:([^']*).*$:\1:g" | sed -E "s: $::g")
+    
+    echo "Options:${options}"
+    basedirectory=$(echo "${line}" | cut -d ' ' -f2- | sed -E "s:[^']*(.*)$:\1:g"| tr -d "'")
+    echo "Directory:${basedirectory}"
+    baseFileName=$(basename "${2}" | sed -E "s:(.*)\.[^\.]*$:\1:g ")
+    echo "baseFileName: ${baseFileName}"
+    if ! $(echo "${options}" | grep -q -E "recognizer"); then
+      echo "Recognizer not found"
+      exit 4
+    fi
+    optionsToParse=$(mktemp)
+    mkdir -p "${basedirectory}/${baseFileName}"
+    if ! $(echo "${options}" | grep -q -E "listener"); then
+        echo "-no-listener" >> ${optionsToParse}
+    fi
+    
+    if $(echo "${options}" | grep -q -E "visitor"); then
+        echo "-visitor" >> ${optionsToParse}
+    fi
+    cleanOptions=$(cat "${optionsToParse}" | tr '\n' ' ')
+    
+    echo "-Dlanguage=${language} ${cleanOptions}-o ${basedirectory}/${baseFileName} ${2}"
+    
+    echo "-Dlanguage=${language} ${cleanOptions}-o ${basedirectory}/${baseFileName} ${2}" >> ${commands}
+    
+    rm ${optionsToParse}
+    echo "Line: ${line}"
+done< <(cat ${1})
+
+while read -r myComm; do
+    echo "antlr4 ${myComm}"
+    eval "antlr4 ${myComm}"
+done< <(cat "${commands}")
+rm ${commands}
